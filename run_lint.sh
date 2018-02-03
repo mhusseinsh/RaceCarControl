30c8a316e6b12ae21e23f74130e1085851350b21
mh806 <mh806@tf.uni-freiburg.de>
2018-02-03 15:22:02 +0100

    cleaning directory

--- run_lint.sh
+++
@@ -1,2 +0,0 @@
-cpplint --counting=detailed $( find . -name *.hpp -or -name *.cpp -or -name *.c -or -name *.h | grep -vE "qpoases" | grep -vE "qpOASES" | grep -vE "acado_" | grep -vE "_export" | grep -vE "export_" )
-
